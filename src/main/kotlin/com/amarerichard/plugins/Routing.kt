5250b0cd9eb83fb0949e5e9306a62473726daede
Tikaani <romik152006@mail.ru>
2023-11-18 22:39:10 +0300

    add new rout

--- src/main/kotlin/com/amarerichard/plugins/Routing.kt
+++ src/main/kotlin/com/amarerichard/plugins/Routing.kt
@@ -9,5 +9,8 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
+        get("/hello") {
+            call.respondText("Hello Tikaani!")
+        }
     }
 }
